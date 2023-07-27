07dc6ce647e1a1e9d9fb3d427b480867b2d9cdbf
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2023-07-27 15:59:58 +0200

    PC

---
+++ Problem Solving Solutions/utopian_tree.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    ht = 0
+    for cc in range(0,n+1):
+        if cc%2 != 0: ht*=2
+        elif cc%2 == 0: ht+=1
+    return ht
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
