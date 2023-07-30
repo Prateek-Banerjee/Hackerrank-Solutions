f229fba7687ee9532163e9cb108f754dfe49e1de
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2023-07-30 19:59:51 +0200

    pc

---
+++ Problem Solving Solutions/viral_advertising.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    recipients,likeCount = 5,0
+    for i in range(1,n+1):
+        likeCount+=math.floor(recipients/2)
+        recipients = 3*(math.floor(recipients/2))
+    return likeCount
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
