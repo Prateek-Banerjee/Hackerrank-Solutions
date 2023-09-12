a866e9eea0d5d6db2e6c2c29e12ae448fa58246c
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2023-09-12 16:04:35 +0200

    pc

---
+++ Problem Solving Solutions/Algorithms/acm_icpc_team.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'acmTeam' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY topic as parameter.
+#
+
+def acmTeam(topic):
+    # Write your code here
+    teams = []
+    for i in range(len(topic)-1):
+        for j in range(i+1,len(topic)):
+            combinations = str(int(topic[i])+int(topic[j]))
+            teams.append(int(len(combinations))-combinations.count('0'))
+    return [max(teams),teams.count(max(teams))]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    topic = []
+
+    for _ in range(n):
+        topic_item = input()
+        topic.append(topic_item)
+
+    result = acmTeam(topic)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
