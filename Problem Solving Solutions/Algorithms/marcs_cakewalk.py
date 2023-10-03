aac28f1cf954da75e1331d9974ed57741f568524
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2023-10-03 17:19:29 +0200

    pc

---
+++ Problem Solving Solutions/Algorithms/marcs_cakewalk.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    # Write your code here
+    reverseCalorie = sorted(calorie, reverse=True)
+    minimumMiles = 0
+    for i in range(len(reverseCalorie)):
+        minimumMiles+=((2**i)*reverseCalorie[i])
+    return minimumMiles
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
