94bbcf483fe3a37031664f52789549e3c7391137
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2024-03-23 05:47:29 +0100

    Java Starts

---
+++ Java Solutions/JavaLoopsTwo.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaLoopsTwo {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int a=0,b=0,n=0;
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            a = in.nextInt();
+            b = in.nextInt();
+            n = in.nextInt();            
+            
+            double sum =0;            
+            for(int j=0;j<n;j++){                                
+                sum += b*Math.pow(2, j);                
+                //int value = (int)(a+sum);
+                System.out.print((int)(a+sum)+" ");            
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
---
+++ Java Solutions/JavaOutputFormatting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                String newX = String.valueOf(x);
+                int lenX = newX.length();
+                System.out.println(s1 + " ".repeat( 18-( s1.concat("0".repeat(3-lenX)).concat(newX).length() ) ) + "0".repeat(3-lenX) + newX);
+            }
+            System.out.println("================================");
+
+    }
+}
