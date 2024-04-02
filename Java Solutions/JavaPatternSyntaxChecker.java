715b71b0334c24e9a23351a90ec023cec6b46389
Prateek Banerjee <106337270+Prateek-Banerjee@users.noreply.github.com>
2024-04-02 19:13:15 +0200

    java commits

---
+++ Java Solutions/JavaPatternSyntaxChecker.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class JavaPatternSyntaxChecker
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int testCases = Integer.parseInt(sc.nextLine());
+        while(testCases>0){
+            String pattern = sc.nextLine();
+              //Write your code
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (Exception e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+        sc.close();
+    }
+}
